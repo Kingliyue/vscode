714d2371e64fb0041baaf81429730db5ebb9c970
Kingliyue <602100452@qq.com>
2021-06-22 00:30:01 +0800

    提交

--- src/views/edu/subject/list.vue
+++ src/views/edu/subject/list.vue
@@ -1,9 +1,57 @@
 <template>
-  <div>
-        
+  <div class="app-container">
+    <el-input v-model="filterText" placeholder="Filter keyword" style="margin-bottom:30px;" />
+
+    <el-tree
+      ref="tree2"
+      :data="data2"
+      :props="defaultProps"
+      :filter-node-method="filterNode"
+      class="filter-tree"
+      default-expand-all
+    />
+
   </div>
 </template>
+
 <script>
+import {getSubject} from '@/api/subject'
+export default {
+
+  data() {
+    return {
+      filterText: '',
+      data2: [],
+      defaultProps: {
+        children: 'children',
+        label: 'title'
+      }
+    }
+  },
+  watch: {
+    filterText(val) {
+      this.$refs.tree2.filter(val)
+    }
+  },
+  created(){
+    this.getSubjectAll()
+  },
+  methods: {
+      getSubjectAll(){
+      getSubject().then(res=>{
+        console.log()
+        console.log(res.data.list)
+        this.data2=res.data.list
+
+      })
+    },
+    filterNode(value, data) {
+      if (!value) return true
+        return data.title.toLowerCase().indexOf(value.toLowerCase()) !== -1
+    },
+  
+  }
+
+}
 </script>
-<style scoped>
-</style>
+
